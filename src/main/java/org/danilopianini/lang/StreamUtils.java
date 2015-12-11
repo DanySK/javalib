ff9e7a824f092a5cb151bacf84724d91c1e8ee5d
Danilo Pianini <danilo.pianini@gmail.com>
2015-12-11 18:38:01 +0100

    Add StreamUtils

---
+++ src/main/java/org/danilopianini/lang/StreamUtils.java
@@ -0,0 +1,31 @@
+package org.danilopianini.lang;
+
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+/**
+ * Complementary utilities for the Java 8 Stream library.
+ */
+public final class StreamUtils {
+
+    private StreamUtils() {
+    }
+
+    /**
+     * Flattens a recursive, non circular data structure into a {@link Stream}.
+     * 
+     * @param target
+     *            the root element of the structure
+     * @param extractor
+     *            the function that maps a node of the structure to a stream of
+     *            its children
+     * @param <E> the type of object
+     * @return a {@link Stream} walking through the data structure
+     */
+    public static <E> Stream<E> flatten(
+            final E target,
+            final Function<? super E, ? extends Stream<? extends E>> extractor) {
+        return Stream.concat(Stream.of(target), extractor.apply(target).flatMap(el -> flatten(el, extractor)));
+    }
+
+}
